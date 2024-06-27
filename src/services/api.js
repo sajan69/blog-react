d15bf452ae8b7f92134849d781cdc5020ee1dfd9
Sajan Adhikari <120996556+sajan69@users.noreply.github.com>
2024-06-27 23:53:58 +0545

    live node try

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://blog-project-node.onrender.com/api'; 
+const API_URL = 'https://blog-project-node.onrender.com'; 
 
 const api = axios.create({
   baseURL: API_URL,
